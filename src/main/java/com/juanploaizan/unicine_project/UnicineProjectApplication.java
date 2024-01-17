1164df0f11ff083315373600d118bcf1bae3ab0b
juanploaizan <jloaizanieto@gmail.com>
2024-01-16 21:24:09 -0500

    initial commit

---
+++ src/main/java/com/juanploaizan/unicine_project/UnicineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.juanploaizan.unicine_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicineProjectApplication.class, args);
+	}
+
+}
